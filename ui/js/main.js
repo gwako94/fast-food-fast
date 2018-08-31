af4610c539643ac79e640739879924945ca822f2
Galgallo Wako <galgallowako254@gmail.com>
2018-08-31 01:09:24 +0000

    [ft-add_order##160078324]Admin menu management page

---
+++ ui/js/main.js
@@ -0,0 +1,8 @@
+function Bar() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
