93678ffde5d0e1dc6d6d371dd02c967dce565ea8
Galgallo Wako <galgallowako254@gmail.com>
2018-10-11 10:51:15 +0000

    [#161146469]fetch order history

---
+++ ui/js/fetch_history.js
@@ -0,0 +1,39 @@
+const history_url = 'https://herokufastfoodapi.herokuapp.com/api/v2/users/orders'
+const history = document.getElementById('order_history')
+
+window.onload = function getHistory (){
+    fetch(history_url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+            'access-token': localStorage.getItem('token')
+        }
+
+    })
+    .then((res) => {
+        status_code = res.status;
+        return res.json();
+    })
+    .then((data) =>{
+        if (status_code === 200){
+            order_history = ""
+            data['Orders'].forEach(order => {
+                console.log(order.cart)
+                order_history+=`
+            <tr>
+                <td>${order.id}</td>
+                <td>
+                    <ul>
+                        <li>${order.cart}</li>
+                    </ul>
+                </td>
+                <td>Ksh. ${order.total}</td>
+                <td>${order.created_at}</td>
+                <td>${order.status}</td>
+            </tr>`
+            });
+        }
+        history.innerHTML += order_history
+    });
+};
