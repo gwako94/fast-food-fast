698081d6290b7e503b94b57a5826ef04fc18b5b6
Galgallo Wako <galgallowako254@gmail.com>
2018-10-18 22:01:57 +0000

    bug fix

--- ui/js/add_menu.js
+++ ui/js/add_menu.js
@@ -2,6 +2,64 @@ const menu_url = 'https://herokufastfoodapi.herokuapp.com/api/v2/menu'
 const menu = document.getElementById('add_menu')
 const success = document.getElementById('success')
 const error = document.getElementById('error')
+const mycontent = document.getElementById('mycontent')
+
+window.onload = function getMenu(){
+    fetch(menu_url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        }
+
+    })
+    .then((res) => {
+        status_code = res.status;
+        return res.json();
+    })
+    .then((data) => {
+        if (status_code === 200){
+            data['Menu'].forEach(menu => {
+            let div = document.createElement('div')
+            div.setAttribute('class', "item")
+            let image = document.createElement('img')
+            image.setAttribute('src', "images/" + menu.image_url)
+            div.appendChild(image)
+            let desc_div = document.createElement('div')
+            desc_div.setAttribute('class', "description")
+            div.appendChild(desc_div)
+            let h3 = document.createElement('h3')
+            h3.setAttribute('id', "item_name")
+            h3.setAttribute('value', menu.item_name)
+            let h3_text = document.createTextNode(menu.item_name)
+            h3.appendChild(h3_text)
+            desc_div.appendChild(h3)
+            let edit_button = document.createElement('button')
+            edit_button.setAttribute('type', "submit")
+            edit_button.setAttribute('id', "edit")
+            let edit_text = document.createTextNode('Edit')
+            edit_button.appendChild(edit_text)
+            let del_button = document.createElement('button')
+            del_button.setAttribute('type', "submit")
+            del_button.setAttribute('id', "delete")
+            let del_text = document.createTextNode('Delete')
+            del_button.appendChild(del_text)
+            div.appendChild(edit_button)
+            div.appendChild(del_button)
+            let span = document.createElement('span')
+            let span_text = document.createTextNode('Ksh. '+ menu.price)
+            span.appendChild(span_text)
+            div.appendChild(span)
+            mycontent.appendChild(div)
+            });
+      
+        }
+
+    })
+
+}
+
+
 
 menu.addEventListener('submit', e => {
     e.preventDefault();
