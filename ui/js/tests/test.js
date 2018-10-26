b61c43723c6f0ff66f3ea261d5ce98ae12340368
Galgallo Wako <galgallowako254@gmail.com>
2018-10-26 00:56:21 +0000

    [#161496758] test register new user

---
+++ ui/js/tests/test.js
@@ -0,0 +1,23 @@
+let request = require('request');
+let expect = require('chai').expect;
+let assert = require('chai').assert;
+
+describe('Register a new user', function(){
+    let user = {
+            "username": "user1",
+            "email": "user1@gmail.com",
+            "password": "@user1"
+        }
+  
+    it('register a new user', function(){
+        request.post({
+            url: "https://herokufastfoodapi.herokuapp.com/api/v2/auth/register",
+            body: user,
+            json: true
+          },
+        function(error, response, body){
+            expect(response.statusCode).to.equal(201);
+            alert(body)
+        });
+    });
+})
